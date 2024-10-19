76c0a07c72b830c79cb3241f12a6888b5200d95d
Elisa Bergamini <bergamini.elisa1@gmail.com>
2024-10-19 16:22:22 +0200

    Tests

--- tests/Feature/AttendeeTest.php
+++ tests/Feature/AttendeeTest.php
@@ -2,18 +2,67 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\Attendee;
 
 class AttendeeTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
-    public function test_example(): void
+    public function setUp(): void
     {
-        $response = $this->get('/');
+        parent::setUp();
+
+        Attendee::factory()->count(2)->create();
+
+        $this->withHeaders([
+            'x-api-key' => env('API_KEY'),
+        ]);
+    }
+
+    public function test_api_attendee_index(): void 
+    {
+        $response = $this->get('/api/attendees');
+
+        $response->assertStatus(200);
+    }
+
+    public function test_api_attendee_show(): void 
+    {
+        $attendee = Attendee::first();
+
+        $response = $this->get('/api/attendees/' . $attendee->id);
+
+        $response->assertStatus(200);
+    }
+
+    public function test_api_attendee_store(): void 
+    {
+        $response = $this->post('/api/attendees', [
+            'firstname' => 'MyAttendee',
+            'lastname' => 'Surname',
+            'email' => 'test@email.com'
+        ]);
+
+        $response->assertStatus(201);
+    }
+
+    public function test_api_attendee_update(): void 
+    {
+        $attendee = Attendee::first();
+
+        $response = $this->put('/api/attendees/' . $attendee->id, [
+            'firstname' => 'Updated Firstname',
+            'lastname' => 'Updated Lastname',
+            'email' => 'testupdate@email.com'
+        ]);
+
+        $response->assertStatus(200);
+    }
+
+    public function test_api_attendee_destroy(): void 
+    {
+        $attendee = Attendee::first();
+
+        $response = $this->delete('/api/attendees/' . $attendee->id);
 
         $response->assertStatus(200);
     }
--- tests/Feature/EventTest.php
+++ tests/Feature/EventTest.php
@@ -9,18 +9,73 @@ class EventTest extends TestCase
 {
     public function setUp(): void
     {
+        parent::setUp();
+
         Event::factory()->count(2)->create();
+
+        $this->withHeaders([
+            'x-api-key' => env('API_KEY'),
+        ]);
     }
 
-    /**
-     * A basic feature test example.
-     */
-    public function test_api_index(): void
+    public function test_api_event_index(): void
     {
         $response = $this->get('/api/events');
 
-        dd($response->dump());
+        $response->assertStatus(200);
+    }
+
+    public function test_api_event_show(): void
+    {
+        $event = Event::first();
+
+        $response = $this->get('/api/events/' . $event->id);
 
         $response->assertStatus(200);
     }
+
+    public function test_api_event_store(): void
+    {
+        $response = $this->post('/api/events', [
+            'title' => 'Test Event',
+            'description' => 'Test Description',
+            'scheduled_at' => '2022-01-01 00:00:00',
+            'location' => 'Test Location',
+            'max_attendees' => 10,
+        ]);
+
+        $response->assertStatus(201);
+    }
+
+    public function test_api_event_update(): void
+    {
+        $event = Event::first();
+
+        $response = $this->put('/api/events/' . $event->id, [
+            'title' => 'Updated Test Event',
+            'description' => 'Updated Test Description',
+            'scheduled_at' => '2022-01-01 00:00:00',
+            'location' => ' Updated Test Location',
+            'max_attendees' => 10,
+        ]);
+
+        $response->assertStatus(200);
+    }
+
+    public function test_api_event_destroy(): void
+    {
+        $event = Event::first();
+
+        $response = $this->delete('/api/events/' . $event->id);
+
+        $response->assertStatus(200);
+    }
+
+
+    public function tearDown(): void
+    {
+        Event::query()->delete();
+        parent::tearDown();
+    }
+
 }
